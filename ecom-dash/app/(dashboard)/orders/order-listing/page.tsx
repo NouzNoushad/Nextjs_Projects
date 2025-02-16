2b745209c4fd54ca17715e614808f26a075a976b
unknown <nhd7noushadazeez@gmail.com>
2025-02-16 22:02:01 +0530

    orders listing

--- ecom-dash/app/(dashboard)/orders/order-listing/page.tsx
+++ ecom-dash/app/(dashboard)/orders/order-listing/page.tsx
@@ -1,7 +1,17 @@
+import OrdersListingCard from '@/components/OrdersListing'
 import React from 'react'
 
 export default function OrdersListing() {
-  return (
-    <div>OrdersListing</div>
-  )
+    return (
+        <main className='px-8 py-5 flex flex-col'>
+            <div className="flex-shrink-0">
+                <h1 className='text-lg font-semibold'>Orders Listing</h1>
+                <p className='text-gray-400 text-xs font-medium'>Home - Sales</p>
+            </div>
+            <div className="mt-6">
+                {/* Orders Listing */}
+                <OrdersListingCard />
+            </div>
+        </main>
+    )
 }
--- ecom-dash/components/OrdersListing.tsx
+++ ecom-dash/components/OrdersListing.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 import Pagination from './Pagination'
 
-export default function ProductsCard() {
+export default function OrdersListingCard() {
     const [currentPage, setCurrentPage] = useState(1)
 
     const items = Array.from({ length: 100 }, (_, i) => i + 1)
@@ -21,8 +21,8 @@ export default function ProductsCard() {
 
     return (
         <div className="px-8 py-9 w-full bg-white rounded-md ring-1 ring-gray-100 space-y-5 shadow-[0px_3px_4px_0px_rgba(0,0,0,0.03)]">
-            {/* Products Header */}
-            <div className="flex items-center justify-between mb-10">
+            {/* Orders Listing Header */}
+            <div className="flex items-center justify-between mb-10 space-x-4">
                 <div className="inline-flex items-center space-x-2 bg-gray-100 rounded-md px-2 py-2">
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-[#888] group-hover:text-blue-400">
@@ -30,16 +30,23 @@ export default function ProductsCard() {
                         </svg>
                     </span>
                     <span>
-                        <input type="text" placeholder='Search Product' className='text-sm p-1 bg-transparent focus:outline-none focus:ring-0' />
+                        <input type="text" placeholder='Search Order' className='text-sm p-1 bg-transparent focus:outline-none focus:ring-0' />
                     </span>
                 </div>
-                <div className="flex w-1/3 items-center justify-end space-x-3">
-                    <div className="relative w-1/2 ">
+                <div className="flex w-full items-center justify-end space-x-3">
+                    <input type="text" className='text-sm bg-gray-100 rounded-md px-3 py-3 focus:outline-none focus:ring-0' placeholder='Pick date range' />
+                    <div className="relative w-[20%]">
                         <select name="" id="" className='w-full px-2 py-3 appearance-none rounded-md bg-gray-100 focus:outline-none focus:ring-0 text-sm'>
                             <option value="">All</option>
-                            <option value="">Published</option>
-                            <option value="">Scheduled</option>
-                            <option value="">Inactive</option>
+                            <option value="">Cancelled</option>
+                            <option value="">Completed</option>
+                            <option value="">Denied</option>
+                            <option value="">Expired</option>
+                            <option value="">Failed</option>
+                            <option value="">Pending</option>
+                            <option value="">Processing</option>
+                            <option value="">Delivered</option>
+                            <option value="">Delivering</option>
                         </select>
                         <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
@@ -47,21 +54,21 @@ export default function ProductsCard() {
                             </svg>
                         </div>
                     </div>
-                    <button className='bg-blue-500 text-white px-5 py-3 rounded-md text-sm font-medium focus:outline-none foucs:ring-0 transition-colors hover:bg-blue-600'>Add Product</button>
+                    <button className='bg-blue-500 text-white px-5 py-3 rounded-md text-sm font-medium focus:outline-none foucs:ring-0 transition-colors hover:bg-blue-600'>Add Order</button>
                 </div>
             </div>
-            {/* Products Table */}
+            {/* Orders Listing Table */}
             <div className="w-full overflow-x-auto">
                 <table className='w-full border-collapse'>
                     <thead>
                         <tr className='border-b-[1px] border-dashed'>
-                            <th className='min-w-[200px] text-start font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Product</span></th>
-                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>SKU</span></th>
-                            <th className='min-w-[70px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Qty</span></th>
-                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Price</span></th>
-                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Rating</span></th>
-                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Status</span></th>
-                            <th className='min-w-[70px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Actions</span></th>
+                            <th className='min-w-[100px] text-start font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Order Id</span></th>
+                            <th className='min-w-[175px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Customer</span></th>
+                            <th className='min-w-[70px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Status</span></th>
+                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Total</span></th>
+                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Date Added</span></th>
+                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Date Modified</span></th>
+                            <th className='min-w-[100px] text-end font-semibold text-[13px] uppercase py-4 text-gray-400'><span>Actions</span></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -69,24 +76,24 @@ export default function ProductsCard() {
                         {
                             paginatedItems.map((item, index) => (
                                 <tr key={index} className={`${index !== 9 ? "border-b-[1px] border-dashed" : ""}`}>
+                                    <td className='py-4'>
+                                        <div className='text-sm font-semibold'>12545</div>
+                                    </td>
                                     <td className='py-4 flex items-center space-x-4'>
-                                        <div className='h-11 w-11 bg-gray-100 rounded-md'></div>
-                                        <div className='text-sm font-semibold'>{`Product ${item}`}</div>
+                                        <div className='h-12 w-12 bg-gray-100 rounded-full'></div>
+                                        <div className='text-sm font-semibold'>{`User ${item}`}</div>
                                     </td>
                                     <td className='py-4 text-end'>
-                                        <div className='text-sm font-semibold text-gray-500'>03877007</div>
+                                        <div className='text-[11px] font-semibold bg-green-100 text-green-500 inline-block p-1 rounded-md'>Completed</div>
+                                    </td>
+                                    <td className='py-4 text-end'>
+                                        <div className='text-sm font-semibold text-gray-500'>$327.00</div>
                                     </td>
                                     <td className='py- text-end'>
-                                        <div className='text-sm font-semibold text-gray-500'>18</div>
+                                        <div className='text-sm font-semibold text-gray-500'>09/02/2025</div>
                                     </td>
                                     <td className='py-4 text-end'>
-                                        <div className='text-sm font-semibold text-gray-500'>286.00</div>
-                                    </td>
-                                    <td className='py-4 text-end'>
-                                        <div className='text-sm font-semibold'>5</div>
-                                    </td>
-                                    <td className='py-4 text-end'>
-                                        <div className='text-[11px] font-semibold bg-green-100 text-green-500 inline-block p-1 rounded-md'>Published</div>
+                                        <div className='text-sm font-semibold text-gray-500'>15/02/2025</div>
                                     </td>
                                     <td className='py-4 text-end'>
                                         <div className='inline-flex items-center justify-end space-x-2 bg-gray-100 px-3 py-2 rounded-md'>
