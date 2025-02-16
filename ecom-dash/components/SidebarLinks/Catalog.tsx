cfcd446e38a05da6d9c8d3ef41fe85f673a0e8f8
unknown <nhd7noushadazeez@gmail.com>
2025-02-16 15:47:12 +0530

    categories

--- ecom-dash/components/SidebarLinks/Catalog.tsx
+++ ecom-dash/components/SidebarLinks/Catalog.tsx
@@ -35,14 +35,17 @@ export default function CatalogLink() {
             </button>
             {/* Sub Nav */}
             <div className={`mt-1 px-4 space-y-1 transition-transform ${showCatalog ? "overflow-y-auto" : "hidden overflow-y-hidden"}`}>
+                {/* Products */}
                 <button onClick={() => handleCatalogChange(CatalogType.Products, "/")} className={`nav-link group group-transition-colors group-duration-300 focus:outline-none focus:ring-0 w-full ${selectedCatalog == CatalogType.Products ? "bg-primary-light" : "bg-transparent"}`}>
                     <span className={`block w-1 h-1 group-hover:bg-white rounded-full ${selectedCatalog == CatalogType.Products ? "bg-white" : "bg-gray-400"}`}></span>
                     <span className={`text-[13px] font-normal group-hover:text-white ${selectedCatalog == CatalogType.Products ? "text-white" : "text-gray-400"}`}>Products</span>
                 </button>
-                <a href="#" className='nav-link group group-transition-colors group-duration-300 focus:bg-primary-light'>
-                    <span className='block w-1 h-1 bg-gray-400 group-focus:bg-white group-hover:bg-white rounded-full'></span>
-                    <span className='text-[13px] font-normal'>Categories</span>
-                </a>
+                {/* Categories */}
+                <button onClick={() => handleCatalogChange(CatalogType.Categories, "/categories")} className={`nav-link group group-transition-colors group-duration-300 focus:outline-none focus:ring-0 w-full ${selectedCatalog == CatalogType.Categories ? "bg-primary-light" : "bg-transparent"}`}>
+                    <span className={`block w-1 h-1 group-hover:bg-white rounded-full ${selectedCatalog == CatalogType.Categories ? "bg-white" : "bg-gray-400"}`}></span>
+                    <span className={`text-[13px] font-normal group-hover:text-white ${selectedCatalog == CatalogType.Categories ? "text-white" : "text-gray-400"}`}>Categories</span>
+                </button>
+                {/* Add Products */}
                 <button onClick={() => handleCatalogChange(CatalogType.AddProduct, "/add-product")} className={`nav-link group group-transition-colors group-duration-300 focus:outline-none focus:ring-0 w-full ${selectedCatalog == CatalogType.AddProduct ? "bg-primary-light" : "bg-transparent"}`}>
                     <span className={`block w-1 h-1 group-hover:bg-white rounded-full ${selectedCatalog == CatalogType.AddProduct ? "bg-white" : "bg-gray-400"}`}></span>
                     <span className={`text-[13px] font-normal group-hover:text-white ${selectedCatalog == CatalogType.AddProduct ? "text-white" : "text-gray-400"}`}>Add Product</span>
@@ -51,10 +54,7 @@ export default function CatalogLink() {
                     <span className='block w-1 h-1 bg-gray-400 group-focus:bg-white group-hover:bg-white rounded-full'></span>
                     <span className='text-[13px] font-normal'>Edit Product</span>
                 </a>
-                <a href="#" className='nav-link group group-transition-colors group-duration-300 focus:bg-primary-light'>
-                    <span className='block w-1 h-1 bg-gray-400 group-focus:bg-white group-hover:bg-white rounded-full'></span>
-                    <span className='text-[13px] font-normal'>Products</span>
-                </a>
+                {/* Add Category */}
                 <button onClick={() => handleCatalogChange(CatalogType.AddCategory, "/add-category")} className={`nav-link group group-transition-colors group-duration-300 focus:outline-none focus:ring-0 w-full ${selectedCatalog == CatalogType.AddCategory ? "bg-primary-light" : "bg-transparent"}`}>
                     <span className={`block w-1 h-1 group-hover:bg-white rounded-full ${selectedCatalog == CatalogType.AddCategory ? "bg-white" : "bg-gray-400"}`}></span>
                     <span className={`text-[13px] font-normal group-hover:text-white ${selectedCatalog == CatalogType.AddCategory ? "text-white" : "text-gray-400"}`}>Add Category</span>
