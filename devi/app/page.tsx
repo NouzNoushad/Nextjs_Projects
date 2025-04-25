7b80941cd2f2979c56a0db5c9147d22220f061e4
unknown <nhd7noushadazeez@gmail.com>
2025-04-25 21:43:43 +0530

    nav

--- devi/app/page.tsx
+++ devi/app/page.tsx
@@ -1,12 +1,30 @@
+import ScrollUpButton from "@/components/scrollUpButton";
+
 export default function Home() {
     return (
         <div>
             {/* Scroll Up Button */}
-            <a href="#" className="fixed bottom-[20px] right-[20px] h-[40px] w-[40px] bg-[#ff4a17] rounded-sm transition-all duration-200">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 text-white font-bold absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
-                </svg>
-            </a>
+            <ScrollUpButton />
+            {/* Header */}
+            <header className="h-[">
+                <a href="#">
+                    <h1>DEWI</h1>
+                </a>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Dropdown</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <button>
+                    Get Started
+                </button>
+            </header>
         </div>
     );
 }
