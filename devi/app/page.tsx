54e4fd434447f3fa97e3ce39359afb790dd52329
unknown <nhd7noushadazeez@gmail.com>
2025-04-26 10:59:34 +0530

    nav bar

--- devi/app/page.tsx
+++ devi/app/page.tsx
@@ -6,24 +6,26 @@ export default function Home() {
             {/* Scroll Up Button */}
             <ScrollUpButton />
             {/* Header */}
-            <header className="h-[">
-                <a href="#">
-                    <h1>DEWI</h1>
-                </a>
-                <nav>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Portfolio</a></li>
-                        <li><a href="#">Team</a></li>
-                        <li><a href="#">Dropdown</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
-                <button>
-                    Get Started
-                </button>
+            <header className="h-[12vh] py-8 fixed top-0 left-0 w-full bg-black z-50 transition-all duration-200">
+                <div className="max-w-[1150px] mx-auto px-6 xl:px-0 text-white flex flex-row items-center justify-between h-full">
+                    <a href="#">
+                        <h1 className="text-3xl font-bold">Dewi</h1>
+                    </a>
+                    <nav className="flex flex-row items-center gap-[35px]">
+                        <ul className="flex flex-row items-center justify-center gap-[35px] uppercase font-semibold text-sm">
+                            <li><a className="relative inline-block after:absolute after:bottom-[-6px] after:left-0 after:h-[0.13rem] after:bg-[#FF4A17] after:w-full" href="#">Home</a></li>
+                            <li><a className="nav-link" href="#">About</a></li>
+                            <li><a className="nav-link" href="#">Services</a></li>
+                            <li><a className="nav-link" href="#">Portfolio</a></li>
+                            <li><a className="nav-link" href="#">Team</a></li>
+                            <li><a className="nav-link" href="#">Dropdown</a></li>
+                            <li><a className="nav-link" href="#">Contact</a></li>
+                        </ul>
+                        <button className="border-2 border-white px-2 py-2 rounded-sm uppercase text-xs w-[9rem] font-medium cursor-pointer transition-colors duration-150 hover:bg-[#FF4A17] hover:text-white hover:border-[#FF4A17]">
+                            Get Started
+                        </button>
+                    </nav>
+                </div>
             </header>
         </div>
     );
