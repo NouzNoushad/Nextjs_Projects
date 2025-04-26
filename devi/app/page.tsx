1aa380d78ec6a71c7a2bafddec07271a03cd4cba
unknown <nhd7noushadazeez@gmail.com>
2025-04-26 11:10:23 +0530

    responsive nav

--- devi/app/page.tsx
+++ devi/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                         <h1 className="text-3xl font-bold">Dewi</h1>
                     </a>
                     <nav className="flex flex-row items-center gap-[35px]">
-                        <ul className="flex flex-row items-center justify-center gap-[35px] uppercase font-semibold text-sm">
+                        <ul className="hidden lg:flex flex-row items-center justify-center gap-[35px] uppercase font-semibold text-sm">
                             <li><a className="relative inline-block after:absolute after:bottom-[-6px] after:left-0 after:h-[0.13rem] after:bg-[#FF4A17] after:w-full" href="#">Home</a></li>
                             <li><a className="nav-link" href="#">About</a></li>
                             <li><a className="nav-link" href="#">Services</a></li>
@@ -21,9 +21,14 @@ export default function Home() {
                             <li><a className="nav-link" href="#">Dropdown</a></li>
                             <li><a className="nav-link" href="#">Contact</a></li>
                         </ul>
-                        <button className="border-2 border-white px-2 py-2 rounded-sm uppercase text-xs w-[9rem] font-medium cursor-pointer transition-colors duration-150 hover:bg-[#FF4A17] hover:text-white hover:border-[#FF4A17]">
-                            Get Started
-                        </button>
+                        <div className="flex flex-row items-center space-x-[20px]">
+                            <button className="border-2 border-white px-2 py-2 rounded-sm uppercase text-xs min-w-[8rem] font-medium cursor-pointer transition-colors duration-150 hover:bg-[#FF4A17] hover:text-white hover:border-[#FF4A17]">
+                                Get Started
+                            </button>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 inline-block lg:hidden">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        </div>
                     </nav>
                 </div>
             </header>
