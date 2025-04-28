2e9c3955ce46f7dd8d3679bb1c0f45bc2ba76b40
unknown <nhd7noushadazeez@gmail.com>
2025-04-28 18:02:19 +0530

    tab view reverse image

--- devi/app/page.tsx
+++ devi/app/page.tsx
@@ -6,7 +6,8 @@ import Portfolio from "@/components/portfolio";
 import ScrollUpButton from "@/components/scrollUpButton";
 import Services2 from "@/components/services_2";
 import Status from "@/components/status";
-import { EmojiBinoculars, EmojiBoxSeam, EmojiBrightnessHigh, EmojiCommand } from "@/lib/icons";
+import Image from 'next/image'
+import { CheckIcon, EmojiBinoculars, EmojiBoxSeam, EmojiBrightnessHigh, EmojiCommand } from "@/lib/icons";
 
 export default function Home() {
     return (
@@ -27,6 +28,7 @@ export default function Home() {
             {/* Features */}
             <div className="bg-white">
                 <div className="container-width py-[3rem]">
+                    {/* Feature Tab */}
                     <ul className="flex flex-row items-center justify-center gap-[20px]">
                         <li className="bg-[#FF4A17] px-[20px] py-[25px] border border-[#FF4A17] text-white flex flex-row items-center gap-[15px] cursor-pointer">
                             <div className="">
@@ -53,7 +55,32 @@ export default function Home() {
                             <h4 className="hidden lg:inline-block text-[#273D4E] font-semibold">Nostrum qui dile node</h4>
                         </li>
                     </ul>
-                    <div className=""></div>
+                    {/* Feature Tab View */}
+                    <div className="mt-[2rem]">
+                        <div className="grid lg:grid-cols-2 grid-cols-1 gap-[20px] lg:gap-[40px] font-medium">
+                            <div className="space-y-5 order-2 lg:order-1">
+                                <h3 className="text-[28px] leading-9 font-bold text-[#273D4E]">Voluptatem dignissimos provident laboris nisi ut aliquip ex ea commodo</h3>
+                                <ul className="space-y-2">
+                                    <li className="flex flex-row items-center gap-2">
+                                        <div><CheckIcon className="size-5" /></div>
+                                        <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></li>
+                                    <li className="flex flex-row items-center gap-2">
+                                        <div><CheckIcon className="inline-block size-5" /></div>
+                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit.</p></li>
+                                    <li className="flex flex-row items-start gap-2">
+                                        <div><CheckIcon className="inline-block size-5" /> </div>
+                                        <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</p></li>
+                                </ul>
+                                <p>
+                                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                                </p>
+                            </div>
+                            <div className="order-1 lg:order-2">
+                                <Image src="/images/about.jpg" alt="about" width={1000} height={1000} className="w-full h-fit" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             {/* Services 2 */}
