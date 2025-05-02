85f4d70187f8fdc41a3a6fb199c39e832d075d85
unknown <nhd7noushadazeez@gmail.com>
2025-05-02 21:46:37 +0530

    feature

--- romyk/components/featured.tsx
+++ romyk/components/featured.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Featured() {
@@ -7,6 +8,44 @@ export default function Featured() {
                 <div className="">
                     <h1 className="relative text-3xl lg:text-4xl font-bold tracking-wider md:after:absolute lg:after:left-[40%] md:after:left-[0%] md:after:bottom-[10px] md:after:h-[0.35rem] md:after:w-[90px] md:after:bg-[#FC95C4] md:after:opacity-50 md:after:rounded-full">Our Ice Cream Services</h1>
                     <p className="py-2">tempor incididunt ut labore et dolore magna aliqua</p>
+                </div>
+                <div className="pt-8 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-[20px]">
+                    <div className="relative px-[20px] py-[20px] shadow-[0px_0px_20px_10px_#ededec]" style={{ backgroundImage: "linear-gradient(0deg, #ffffff 41%, #f2f2f0 41%)" }}>
+                        <div className="min-h-[210px] text-center pb-[20px] flex items-center justify-center"><Image src="/images/img-1.png" alt="" width={1000} height={1000} className="w-[180px] h-[180px]" /></div>
+                        <div className="w-[80px] h-[80px] bg-white rounded-full flex items-center justify-center italic font-bold text-2xl mx-auto py-[18px] px-[15px] mt-[-20px]">$10</div>
+                        <h6 className="text-center font-bold text-[22px] text-[#373736]">Strawberry Ice Cream</h6>
+                        <div className="mt-[20px] text-center"><p className='inline-block min-w-[150px] rounded-[40px] text-base py-[8px] px-[4px] text-[#fc95c4]' style={{ border: "1px solid #fc95c4" }}>Add To Cart</p></div>
+                    </div>
+                    <div className="relative px-[20px] py-[20px] shadow-[0px_0px_20px_10px_#ededec]" style={{ backgroundImage: "linear-gradient(0deg, #ffffff 41%, #f2f2f0 41%)" }}>
+                        <div className="min-h-[210px] text-center pb-[20px] flex items-center justify-center"><Image src="/images/img-4.png" alt="" width={1000} height={1000} className="w-[180px] h-[180px]" /></div>
+                        <div className="w-[80px] h-[80px] bg-white rounded-full flex items-center justify-center italic font-bold text-2xl mx-auto py-[18px] px-[15px] mt-[-20px]">$10</div>
+                        <h6 className="text-center font-bold text-[22px] text-[#373736]">Strawberry Ice Cream</h6>
+                        <div className="mt-[20px] text-center"><p className='inline-block min-w-[150px] rounded-[40px] text-base py-[8px] px-[4px] text-[#fc95c4]' style={{ border: "1px solid #fc95c4" }}>Add To Cart</p></div>
+                    </div>
+                    <div className="relative px-[20px] py-[20px] shadow-[0px_0px_20px_10px_#ededec]" style={{ backgroundImage: "linear-gradient(0deg, #ffffff 41%, #f2f2f0 41%)" }}>
+                        <div className="min-h-[210px] text-center pb-[20px] flex items-center justify-center"><Image src="/images/img-1.png" alt="" width={1000} height={1000} className="w-[180px] h-[180px]" /></div>
+                        <div className="w-[80px] h-[80px] bg-white rounded-full flex items-center justify-center italic font-bold text-2xl mx-auto py-[18px] px-[15px] mt-[-20px]">$10</div>
+                        <h6 className="text-center font-bold text-[22px] text-[#373736]">Strawberry Ice Cream</h6>
+                        <div className="mt-[20px] text-center"><p className='inline-block min-w-[150px] rounded-[40px] text-base py-[8px] px-[4px] text-[#fc95c4]' style={{ border: "1px solid #fc95c4" }}>Add To Cart</p></div>
+                    </div>
+                    <div className="relative px-[20px] py-[20px] shadow-[0px_0px_20px_10px_#ededec]" style={{ backgroundImage: "linear-gradient(0deg, #ffffff 41%, #f2f2f0 41%)" }}>
+                        <div className="min-h-[210px] text-center pb-[20px] flex items-center justify-center"><Image src="/images/img-3.png" alt="" width={1000} height={1000} className="w-[180px] h-[180px]" /></div>
+                        <div className="w-[80px] h-[80px] bg-white rounded-full flex items-center justify-center italic font-bold text-2xl mx-auto py-[18px] px-[15px] mt-[-20px]">$10</div>
+                        <h6 className="text-center font-bold text-[22px] text-[#373736]">Strawberry Ice Cream</h6>
+                        <div className="mt-[20px] text-center"><p className='inline-block min-w-[150px] rounded-[40px] text-base py-[8px] px-[4px] text-[#fc95c4]' style={{ border: "1px solid #fc95c4" }}>Add To Cart</p></div>
+                    </div>
+                    <div className="relative px-[20px] py-[20px] shadow-[0px_0px_20px_10px_#ededec]" style={{ backgroundImage: "linear-gradient(0deg, #ffffff 41%, #f2f2f0 41%)" }}>
+                        <div className="min-h-[210px] text-center pb-[20px] flex items-center justify-center"><Image src="/images/img-4.png" alt="" width={1000} height={1000} className="w-[180px] h-[180px]" /></div>
+                        <div className="w-[80px] h-[80px] bg-white rounded-full flex items-center justify-center italic font-bold text-2xl mx-auto py-[18px] px-[15px] mt-[-20px]">$10</div>
+                        <h6 className="text-center font-bold text-[22px] text-[#373736]">Strawberry Ice Cream</h6>
+                        <div className="mt-[20px] text-center"><p className='inline-block min-w-[150px] rounded-[40px] text-base py-[8px] px-[4px] text-[#fc95c4]' style={{ border: "1px solid #fc95c4" }}>Add To Cart</p></div>
+                    </div>
+                    <div className="relative px-[20px] py-[20px] shadow-[0px_0px_20px_10px_#ededec]" style={{ backgroundImage: "linear-gradient(0deg, #ffffff 41%, #f2f2f0 41%)" }}>
+                        <div className="min-h-[210px] text-center pb-[20px] flex items-center justify-center"><Image src="/images/img-5.png" alt="" width={1000} height={1000} className="w-[180px] h-[180px]" /></div>
+                        <div className="w-[80px] h-[80px] bg-white rounded-full flex items-center justify-center italic font-bold text-2xl mx-auto py-[18px] px-[15px] mt-[-20px]">$10</div>
+                        <h6 className="text-center font-bold text-[22px] text-[#373736]">Strawberry Ice Cream</h6>
+                        <div className="mt-[20px] text-center"><p className='inline-block min-w-[150px] rounded-[40px] text-base py-[8px] px-[4px] text-[#fc95c4]' style={{ border: "1px solid #fc95c4" }}>Add To Cart</p></div>
+                    </div>
                 </div>
             </div>
         </div>
