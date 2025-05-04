449dba3eccf2f5cf074d63dd6672bb8b7275af42
unknown <nhd7noushadazeez@gmail.com>
2025-05-04 16:13:35 +0530

    portfolio setup

--- portfolio/app/(portfolio)/page.tsx
+++ portfolio/app/(portfolio)/page.tsx
@@ -1,17 +1,23 @@
 import AboutMe from "@/components/about";
 import Contact from "@/components/contact";
+import Footer from "@/components/footer";
 import Main from "@/components/main";
+import Navbar from "@/components/navbar";
 import Projects from "@/components/projects";
 import Youtube from "@/components/youtube";
 
 export default function Home() {
     return (
         <>
-            <Main />
-            <AboutMe />
-            <Projects />
-            <Youtube />
-            <Contact />
+            <Navbar />
+            <div className="lg:ml-[300px] px-8">
+                <Main />
+                <AboutMe />
+                <Projects />
+                <Youtube />
+                <Contact />
+                <Footer />
+            </div>
         </>
     );
 }
--- portfolio/components/navbar.tsx
+++ portfolio/components/navbar.tsx
@@ -13,42 +13,50 @@ export default function Navbar() {
     }
 
     return (
-        <header className='fixed top-0 left-0 w-full primary-color py-3 shadow-sm transition-shadow duration-500 z-[1000]'>
-            <div className="container-lg">
-                <div className="flex flex-row items-center justify-between relative">
-                    <div className="flex flex-row items-center gap-3">
-                        <Image src={'/profile.jpg'} height={45} width={45} alt='profile' className='border-2 border-white rounded-full px-[0.5] py-[0.5]' />
-                        <h1 className='text-2xl font-bold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>Noushad</h1>
-                    </div>
-                    <ul className={`flex lg:flex-row flex-col items-center justify-center lg:justify-end gap-4 fixed top-0 primary-color h-full w-full lg:relative lg:top-0 lg:left-0 lg:bg-transparent z-50 transition-all duration-500 ${toggleMenu ? 'right-0' : 'right-[-100%]'}`}>
-                        <li><button onClick={handleToggle}>
-                            <a href="#home" className='nav-link'>Home</a>
-                        </button>
-                        </li>
-                        <li><button onClick={handleToggle}>
-                            <a href="#about" className='nav-link'>About</a>
-                        </button>
-                        </li>
-                        <li><button onClick={handleToggle}>
-                            <a href="#project" className='nav-link'>Project</a>
-                        </button>
-                        </li>
-                        <li><button onClick={handleToggle}>
-                            <a href="#youtube" className='nav-link'>Youtube</a>
-                        </button>
-                        </li>
-                        <li><button onClick={handleToggle}>
-                            <a href="#contact" className='nav-link'>Contact</a>
-                        </button>
-                        </li>
-                        <button onClick={handleToggle} className={`inline-block lg:hidden border-2 border-white px-1 py-1 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.3)] fixed top-5 ${toggleMenu ? 'right-5' : 'right-[-100%]'}`}>
-                            <CloseIcon className='size-5 text-white' />
-                        </button>
-                    </ul>
-                    <button onClick={handleToggle} className="inline-block lg:hidden border-2 border-white px-1 py-1 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.3)]">
-                        <MenuIcon className='size-5' />
-                    </button>
+        <header className='w-[300px] fixed top-0 lg:left-0 left-[-100%] bottom-0 overflow-y-auto px-[15px]  z-50 transition-all duration-300 primary-color border-r-4 border-white'>
+            <div className="flex flex-col items-center justify-between relative pt-5">
+                <div className="flex flex-col items-center gap-3 w-full py-5">
+                    <Image src={'/profile.jpg'} height={100} width={100} alt='profile' className='border-4 border-white rounded-full px-[0.5] py-[0.5]' />
+                    <h1 className='text-2xl font-bold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>Noushad</h1>
                 </div>
+                <ul className={`mt-8 flex flex-col justify-center lg:justify-end gap-4 fixed top-0 primary-color h-full w-full lg:relative lg:top-0 lg:left-0 lg:bg-transparent z-50 transition-all duration-500 ${toggleMenu ? 'right-0' : 'right-[-100%]'}`}>
+                    <li><button onClick={handleToggle} className='nav-btn'>
+                        <a href="#home" className='nav-link'>
+                            <i className="bi bi-house-fill navicon mr-[10px] text-white text-[20px]"></i>
+                            Home</a>
+                    </button>
+                    </li>
+                    <li><button onClick={handleToggle} className='nav-btn'>
+                        <a href="#about" className='nav-link'>
+                            <i className="bi bi-person-fill navicon mr-[10px] text-white text-[20px]"></i>
+                            About</a>
+                    </button>
+                    </li>
+                    <li><button onClick={handleToggle} className='nav-btn'>
+                        <a href="#project" className='nav-link'>
+                            <i className="bi bi-file-earmark-text-fill navicon mr-[10px] text-white text-[20px]"></i>
+                            Project</a>
+                    </button>
+                    </li>
+                    <li><button onClick={handleToggle} className='nav-btn'>
+                        <a href="#youtube" className='nav-link'>
+                            <i className="bi bi-youtube navicon mr-[10px] text-white text-[20px]"></i>
+                            Youtube</a>
+                    </button>
+                    </li>
+                    <li><button onClick={handleToggle} className='nav-btn'>
+                        <a href="#contact" className='nav-link'>
+                            <i className="bi bi-envelope-fill navicon mr-[10px] text-white text-[20px]"></i>
+                            Contact</a>
+                    </button>
+                    </li>
+                    <button onClick={handleToggle} className={`inline-block lg:hidden border-2 border-white px-1 py-1 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.3)] fixed top-5 ${toggleMenu ? 'right-5' : 'right-[-100%]'}`}>
+                        <CloseIcon className='size-5 text-white' />
+                    </button>
+                </ul>
+                <button onClick={handleToggle} className="inline-block lg:hidden border-2 border-white px-1 py-1 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.3)]">
+                    <MenuIcon className='size-5' />
+                </button>
             </div>
         </header>
     )
