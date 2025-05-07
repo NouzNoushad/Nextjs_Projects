65a506f8602754efe76d0144072624a31c09a69c
unknown <nhd7noushadazeez@gmail.com>
2025-05-07 20:34:00 +0530

    about initial

--- port/components/main.tsx
+++ port/components/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainHome from './home'
+import About from './about'
 
 export default function Main() {
 
     return (
         <div className="lg:ml-[300px] bg-white">
             <MainHome />
+            <About />
         </div>
     )
 }
