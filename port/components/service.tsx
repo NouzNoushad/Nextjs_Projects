97b157a0837d08bee7171ebed5c656c93feb76a2
unknown <nhd7noushadazeez@gmail.com>
2025-05-10 21:31:30 +0530

    service

--- port/components/service.tsx
+++ port/components/service.tsx
@@ -9,7 +9,50 @@ export default function Service() {
                     <p className='pt-[20px]'>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
                 </div>
                 <div className="mt-8">
-                    <div className=""></div>
+                    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-[30px]">
+                        <div className="flex flex-row items-start gap-[15px] group cursor-pointer">
+                            <div className="flex-shrink-0 w-[54px] h-[54px] bg-[#149DDD] rounded-full flex items-center justify-center cursor-pointer" style={{ border: '1px solid #149DDD' }}><i className="bi bi-briefcase text-white text-2xl"></i></div>
+                            <div className='flex-1'>
+                                <h4 className="text-[17px] font-bold tracking-wide pb-[10px] transition-colors duration-300 group-hover:text-[#149DDD]"><a href="#" className="">Lorem Ipsum</a></h4>
+                                <p className="text-sm leading-[24px]">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row items-start gap-[15px] group cursor-pointer">
+                            <div className="flex-shrink-0 w-[54px] h-[54px] bg-[#149DDD] rounded-full flex items-center justify-center" style={{ border: '1px solid #149DDD' }}><i className="bi bi-card-checklist text-white text-2xl"></i></div>
+                            <div className='flex-1'>
+                                <h4 className="text-[17px] font-bold tracking-wide pb-[10px] transition-colors duration-300 group-hover:text-[#149DDD]"><a href="#" className="">Dolor Sitema</a></h4>
+                                <p className="text-sm leading-[24px]">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row items-start gap-[15px] group cursor-pointer">
+                            <div className="flex-shrink-0 w-[54px] h-[54px] bg-[#149DDD] rounded-full flex items-center justify-center" style={{ border: '1px solid #149DDD' }}><i className="bi bi-bar-chart text-white text-2xl"></i></div>
+                            <div className='flex-1'>
+                                <h4 className="text-[17px] font-bold tracking-wide pb-[10px] transition-colors duration-300 group-hover:text-[#149DDD]"><a href="#" className="">Sed ut perspiciatis</a></h4>
+                                <p className="text-sm leading-[24px]">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row items-start gap-[15px] group cursor-pointer">
+                            <div className="flex-shrink-0 w-[54px] h-[54px] bg-[#149DDD] rounded-full flex items-center justify-center" style={{ border: '1px solid #149DDD' }}><i className="bi bi-binoculars text-white text-2xl"></i></div>
+                            <div className='flex-1'>
+                                <h4 className="text-[17px] font-bold tracking-wide pb-[10px] transition-colors duration-300 group-hover:text-[#149DDD]"><a href="#" className="">Magni Dolores</a></h4>
+                                <p className="text-sm leading-[24px]">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row items-start gap-[15px] group cursor-pointer">
+                            <div className="flex-shrink-0 w-[54px] h-[54px] bg-[#149DDD] rounded-full flex items-center justify-center" style={{ border: '1px solid #149DDD' }}><i className="bi bi-brightness-high text-white text-2xl"></i></div>
+                            <div className='flex-1'>
+                                <h4 className="text-[17px] font-bold tracking-wide pb-[10px] transition-colors duration-300 group-hover:text-[#149DDD]"><a href="#" className="">Nemo Enim</a></h4>
+                                <p className="text-sm leading-[24px]">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row items-start gap-[15px] group cursor-pointer">
+                            <div className="flex-shrink-0 w-[54px] h-[54px] bg-[#149DDD] rounded-full flex items-center justify-center" style={{ border: '1px solid #149DDD' }}><i className="bi bi-calendar4-week text-white text-2xl"></i></div>
+                            <div className='flex-1'>
+                                <h4 className="text-[17px] font-bold tracking-wide pb-[10px] transition-colors duration-300 group-hover:text-[#149DDD]"><a href="#" className="">Eiusmod Tempor</a></h4>
+                                <p className="text-sm leading-[24px]">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
